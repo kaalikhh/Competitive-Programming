1c8acdb4c9482d7c19d90f2bd122301b1aa4619f
Saurabh Yadav <saurabhy789@gmail.com>
2018-07-10 15:45:40 +0200

    Good String code

---
+++ Good String/main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<limits.h>
+#include<string.h>
+int min(int a,int b){
+return a>b?b:a;
+}
+main(){
+int t,i,r;
+char st[100001];
+scanf("%d",&t);
+while(t){
+
+scanf("%s",st);
+r=strlen(st);
+int front=-1,end=-1,mini=INT_MAX;
+char c='a';
+while(c<='z'){
+for(i=0;i<r;i++){
+if(st[i]==c){
+front=i;
+break;
+}
+}
+for(i=r-1;i>=0;i--){
+if(st[i]==c){
+end=r-i-1;
+break;
+}
+}
+if(front!=-1&&end!=-1&&front+end+2<=r)
+mini=min(front+end,mini);
+c++;
+
+}
+
+
+
+if(mini==INT_MAX)
+    printf("-1\n");
+else
+printf("%d\n",mini);
+
+
+t--;
+}
+}
